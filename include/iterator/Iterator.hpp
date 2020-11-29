517f04e332e4d9cf06607b029dd3ff40655a9676
Vincent Scavinner <vscavinner@gmail.com>
2020-11-29 23:10:19 +0100

    Syntax update

--- include/iterator/Iterator.hpp
+++ include/iterator/Iterator.hpp
@@ -15,22 +15,22 @@ namespace iterator
     public:
         typedef typename std::vector<T>::iterator iter_type;
         /// \brief Constructor.
-        Iterator(U *p_data, bool reverse = false) : m_p_data(p_data) { m_it = m_p_data->m_data.begin(); };
+        Iterator(U *p_data, bool reverse = false) : m_data(p_data) { m_it = m_data->m_data.begin(); };
 
         /// \brief Set the iterator to point to the first element in the vector.
-        void first() { m_it = m_p_data->m_data.begin(); }
+        void first() { m_it = m_data->m_data.begin(); }
 
         /// \brief Set the iterator to the next element in the vector.
         void next() { m_it++; };
 
         /// \brief Set the iterator to the element past the end of the sequence.
-        inline bool isDone() { return (m_it == m_p_data->m_data.end()); };
+        inline bool isDone() { return (m_it == m_data->m_data.end()); };
 
         /// \brief Return the current element.
         inline iter_type current() { return m_it; };
 
     private:
-        U *m_p_data; /*!< A pointer to the data of the container. */
+        U *m_data; /*!< A pointer to the data of the container. */
 
         iter_type m_it; /*!< The vector iterator. */
     };
